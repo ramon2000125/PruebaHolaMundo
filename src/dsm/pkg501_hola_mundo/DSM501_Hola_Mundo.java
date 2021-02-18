a37c62ca517109291d03a355c9e288610b1032fd
Ramon Muñoz <ramonmunozhernandez125@gmail.com>
2021-02-18 13:57:15 -0600

    Esta es mi primera version

---
+++ src/dsm/pkg501_hola_mundo/DSM501_Hola_Mundo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsm.pkg501_hola_mundo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ramon
+ */
+public class DSM501_Hola_Mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola Mundo");
+        
+    }
+    
+}
